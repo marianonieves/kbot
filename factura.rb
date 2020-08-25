f4d577959e4f3670c4b41819074521dd814fef83
marianonieves <mariano.nieves@gmail.com>
2020-08-25 17:15:47 -0300

    agrego linea de impuestos, known bug:no valido si el estado esta en el hash

--- factura.rb
+++ factura.rb
@@ -6,26 +6,35 @@ class Factura
         price=0 if price==nil
         state="CA" if state==nil
 
-        if ARGV.length < 2
-            raise "######## ERROR: missing parameter ######## \n \n $ ruby factura.rb [unit] [price] [state]\n\n"
+        if ARGV.length < 2 || unit < 1 || price < 1
+            raise "######## ERROR: missing parameter ######## \n \n $ ruby factura.rb <unit> <price> [state=CA]\n\n"
             return
         end
 
         @unit=unit
         @price=price
-        @state=state
+        @state=state.upcase
+
+        @impuestos={ "UT"=> 6.85,  "NV"=> 8.0,  "TX"=> 6.25,  "AL"=> 4.00,  "CA"=> 8.25 }
+        @descuentos={ 1000=> 3, 5000=>5, 7000=>7, 10000=>10, 50000=>15 }
+        
     end
 
     def formatedData()
-        " formated data "
+        subtotal = @unit * @price
+        taxes_percentage = @impuestos[@state]
+        taxes_money = subtotal*taxes_percentage /100
+
+"       #{@unit} * $#{@price} = $#{subtotal}
+        #{@state}(%#{taxes_percentage}) = $#{taxes_money}
+        DTO(%0) = $0
+        Total = $52"        
     end 
+
 end
 
 
-unit = ARGV[0]
-price = ARGV[1]
-state = ARGV[2]
-myFactura = Factura.new(unit, price, state)
+myFactura = Factura.new(ARGV[0].to_f, ARGV[1].to_f, ARGV[2])
 puts myFactura.formatedData()
 
 
