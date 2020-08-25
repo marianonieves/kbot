83fea03c6c0ad4088e8b6f2b19f34afc2009f878
marianonieves <mariano.nieves@gmail.com>
2020-08-25 16:46:14 -0300

    agrego la clase Factura y el metodo que devuelve la info para  mostrar en pantalla

--- factura.rb
+++ factura.rb
@@ -1,1 +1,33 @@
-puts “hola, mundo”
+class Factura
+
+    def initialize(unit, price, state)
+        
+        unit=0 if unit==nil
+        price=0 if price==nil
+        state="CA" if state==nil
+
+        if ARGV.length < 2
+            raise "######## ERROR: missing parameter ######## \n \n $ ruby factura.rb [unit] [price] [state]\n\n"
+            return
+        end
+
+        @unit=unit
+        @price=price
+        @state=state
+    end
+
+    def formatedData()
+        " formated data "
+    end 
+end
+
+
+unit = ARGV[0]
+price = ARGV[1]
+state = ARGV[2]
+myFactura = Factura.new(unit, price, state)
+puts myFactura.formatedData()
+
+
+
+
